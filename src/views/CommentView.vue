342fb8e3773ab3d682bc475d55ffc3e4a7479761
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-26 10:45:20 -0300

    feat: adding comments views

---
+++ src/views/CommentView.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="commentview-main">
+    <div class="commentview-container">
+      <router-link class="homeview-main__table__data__link" to="/newcomment">
+        <v-btn color="primary" elevation="7">Adicionar comentário +</v-btn>
+      </router-link>
+      <CommentList />
+    </div>
+  </main>
+</template>
+
+<script>
+import CommentList from "../components/CommentList";
+import AddBtn from "../components/AddBtn";
+
+export default {
+  name: "commentview",
+  components: {
+    CommentList,
+    AddBtn,
+  },
+};
+</script>
+
+<style>
+.commentview-main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3rem;
+}
+.commentview-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 100%;
+  max-width: 720px;
+}
+</style>
---
+++ src/views/NewCommentView.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="new-comment">
+    <ModelForm :fields="formFields" model="comments" route="/comments" />
+  </main>
+</template>
+<script>
+import ModelForm from "../components/ModelForm";
+
+export default {
+  name: "NewCommentView",
+  components: {
+    ModelForm,
+  },
+  data() {
+    return {
+      formFields: {
+        post_id: "text",
+        owner_username: "text",
+        text: "textarea",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.new-comment {
+  padding: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
